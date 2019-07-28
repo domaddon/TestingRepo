55b14a71cd595f09b6a3dc63f94841d2dd3a2edc
Domeshwar Singh <domeshwar.singh@orange.com>
2019-07-28 17:51:29 +0530

    Add Inida

--- HelloOrange.java
+++ HelloOrange.java
@@ -1,5 +1,5 @@
 class HelloOrange {  
     public static void main(String args[]){  
-     System.out.println("Hello Orange Testing");  
+     System.out.println("Hello Orange Testing India");  
     }  
 }  
